0476af7e306ec4cc574b615c2f91ff54b81a7e7e
Abhijit Das <150443491+dasabhijit2004@users.noreply.github.com>
2025-02-14 20:16:13 +0530

    adding Q2 to Q10

---
+++ Set-01/Q5.java
@@ -0,0 +1,9 @@
+// 1, 4, 9, 16, 25, 36, 49, ....................
+
+public class Q5 {
+    public static void main(String[] args) {
+        for(int i = 1; i<8; i++){
+            System.out.print(i*i+" ");
+        }
+    }
+}
---
+++ Set-01/Q6.java
@@ -0,0 +1,9 @@
+// 1, 2, 4, 8, 16, 32, 64, ....................
+
+public class Q6 {
+    public static void main(String[] args) {
+        for(int i = 0; i<7; i++){
+            System.out.print((int)Math.pow(2, i)+" ");
+        }
+    }
+}
