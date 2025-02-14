e7f6eaa7805a8e4603225daa82f09c8e0f67c78f
Abhijit Das <150443491+dasabhijit2004@users.noreply.github.com>
2025-02-14 20:15:15 +0530

    Create Set-01 & Q1.java

---
+++ Set-01/Q1.java
@@ -0,0 +1,39 @@
+// display prime numbers between two intervals.
+
+import java.util.Scanner;
+
+public class Q1{
+
+    static int prime(int num){
+        if(num < 2){
+            return -1;
+        }
+        else if(num > 2){
+            for(int i = 2; i<=num/2; i++){
+                if(num % i == 0){
+                    return -1;
+                }
+            }
+            return 1;
+        }
+        else if (num == 2){
+            return 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the lower value: ");
+        int low = sc.nextInt();
+        System.out.println("Enter the upperr value: ");
+        int high = sc.nextInt();
+
+        for(int i = low; i<=high; i++){
+            if(prime(i) == 1){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
